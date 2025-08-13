067c0e9634d4bc21e62c5d2629c8813eb8c5534f
Hien Phan <dinhienhy@gmail.com>
2025-08-13 10:48:41 +0700

    Fix ActiveTradingSignals Timestamp column type from text to timestamp without time zone

---
+++ Migrations/20250813034700_FixTimestampColumnType.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingSignalsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTimestampColumnType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // PostgreSQL: convert Timestamp column from text to timestamp (without time zone)
+            migrationBuilder.Sql(@"
+                -- Optional safety backup
+                CREATE TABLE IF NOT EXISTS \"ActiveTradingSignals_ts_backup\" AS SELECT * FROM \"ActiveTradingSignals\";
+
+                -- Convert text to timestamp (without time zone)
+                ALTER TABLE \"ActiveTradingSignals\"
+                ALTER COLUMN \"Timestamp\" TYPE timestamp WITHOUT time zone USING \"Timestamp\"::timestamp;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revert back to text if needed
+            migrationBuilder.Sql(@"
+                ALTER TABLE \"ActiveTradingSignals\"
+                ALTER COLUMN \"Timestamp\" TYPE text USING \"Timestamp\"::text;
+
+                DROP TABLE IF EXISTS \"ActiveTradingSignals_ts_backup\";
+            ");
+        }
+    }
+}
