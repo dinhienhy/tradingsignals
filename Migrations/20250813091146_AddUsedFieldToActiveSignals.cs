50c8c2be81675b9833301795de42402c294363e1
Hien Phan <dinhienhy@gmail.com>
2025-08-13 16:26:24 +0700

    Add Used field to ActiveTradingSignals

---
+++ Migrations/20250813091146_AddUsedFieldToActiveSignals.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingSignalsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsedFieldToActiveSignals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Used",
+                table: "ActiveTradingSignals",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Used",
+                table: "ActiveTradingSignals");
+        }
+    }
+}
